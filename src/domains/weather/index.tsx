2a674ed7d2ccf169944c1de741a6c183deee6238
JustHo <zalop3128@kakao.com>
2025-02-25 22:54:04 +0900

    Weather markup

--- src/domains/weather/index.tsx
+++ src/domains/weather/index.tsx
@@ -1,23 +1,26 @@
 import { WeatherAdapterInterface } from "@/api/weather/types";
 import { FC } from "react";
 import { mergeForecastWithShortTermForecast } from "./utils";
+import { LiveSection } from "./LiveSection";
+import { ForecastSection } from "./ForecastSection";
+import { Header } from "./Header";
 
 interface Props {
   live: Awaited<ReturnType<WeatherAdapterInterface["live"]>>;
   today_temperature: Awaited<ReturnType<WeatherAdapterInterface["todayTemperature"]>>;
   merged_forecast: ReturnType<typeof mergeForecastWithShortTermForecast>;
+  update_time: string;
 }
 
 const WeatherMain: FC<Props>  = (props) => {
 
-  const { live , today_temperature, merged_forecast} = props;
-
-  console.log(live, today_temperature);
-  console.log(merged_forecast);
+  const { live , today_temperature, merged_forecast, update_time} = props;
 
   return(
     <main>
-      <h1>Weatehr</h1>
+      <Header update_time={update_time} />
+      <LiveSection live={live} today_temperature={today_temperature} />
+      <ForecastSection forecast_list={merged_forecast}/>
     </main>
   );
 };

fe49c340ec5e1362283c424eeb4d4d380d16c914
JustHo <zalop3128@kakao.com>
2025-02-26 22:13:54 +0900

    gen ai image

--- src/domains/weather/index.tsx
+++ src/domains/weather/index.tsx
@@ -10,14 +10,16 @@ interface Props {
   today_temperature: Awaited<ReturnType<WeatherAdapterInterface["todayTemperature"]>>;
   merged_forecast: ReturnType<typeof mergeForecastWithShortTermForecast>;
   update_time: string;
+  image: string | undefined;
 }
 
 const WeatherMain: FC<Props>  = (props) => {
 
-  const { live , today_temperature, merged_forecast, update_time} = props;
+  const { live , today_temperature, merged_forecast, update_time, image} = props;
 
   return(
     <main>
+      <img src={ `data:image/jpeg;base64,${image}`} alt="" />
       <Header update_time={update_time} />
       <LiveSection live={live} today_temperature={today_temperature} />
       <ForecastSection forecast_list={merged_forecast}/>
--- src/pages/weather/index.tsx
+++ src/pages/weather/index.tsx
@@ -1,8 +1,9 @@
 import { Weather, WeatherAdapter } from "@/api/weather";
 import { WeatherMain } from "@/domains/weather";
-import { mergeForecastWithShortTermForecast } from "@/domains/weather/utils";
+import { getRainyType, mergeForecastWithShortTermForecast } from "@/domains/weather/utils";
 import dayjs from "dayjs";
 import { GetStaticProps } from "next";
+import OpenAI from "openai";
 import { ComponentProps, FC } from "react";
 
 interface Props extends ComponentProps<typeof WeatherMain> {}
@@ -24,12 +25,32 @@ export const getStaticProps: GetStaticProps<Props> = async () => {
 
   const merged_forecast = mergeForecastWithShortTermForecast(forecast, short_term_forecast);
 
+  const openai = new OpenAI({ apiKey: process.env.OPEN_AI_KEY });
+
+  const temperature = live.T1H.obsrValue;
+  const sky = getRainyType(live.PTY.obsrValue);
+
+  const prompt = `${sky} 하늘 풍경 사진을 그려봐`;
+
+  const images = await openai.images.generate({
+    model: "dall-e-2",
+    n: 1,
+    quality: "standard",
+    size: '256x256',
+    prompt: prompt,
+    response_format: "b64_json"
+
+  });
+
+  const image = images.data.at(0)?.b64_json;
+
   return {
     props: {
       live,
       today_temperature,
       merged_forecast,
       update_time: dayjs().format("YYYY-MM-DD HH:mm:ss"),
+      image,
     },
   };
 }
